df758318ce83daab605cb2ed13a483f53c043130
Govind2k5 <govindaraj2k5@gmail.com>
2026-08-18 00:25:03 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/lendingplatform/EmbeddedLendingPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.lendingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedLendingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedLendingPlatformApplication.class, args);
+    }
+}
